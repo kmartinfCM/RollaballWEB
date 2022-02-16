3db4755846775e3ff5ceb659ee498f1fe42aac57
kmartinfCM <mfernandez@certainmeasures.com>
2022-02-16 16:21:48 -0500

    change to app

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('port', port)
 const server = http.createServer(app)
 
 //App use folder
-app.use(express.static('app2'))
+app.use(express.static('app'))
 
 //Terminal Message
 server.listen(port, () => console.log("server has started! port: " + port) );
@@ -26,7 +26,7 @@ server.listen(port, () => console.log("server has started! port: " + port) );
 //app.get("/", (req, res) => {res.send("Hello, here is your Unity WebGL game:");});
 
 //App Page
-app.get("/", (req, res) => {res.sendFile(path.join(__dirname, '/app2/index.html'));});
+app.get("/", (req, res) => {res.sendFile(path.join(__dirname, '/app/index.html'));});
 
 
 //Data Page
